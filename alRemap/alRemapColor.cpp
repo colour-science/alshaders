18ee5c445f357daf377ecc7ec4f5b845d11f5e26
Anders Langlands <anderslanglands@gmail.com>
2012-11-21 17:14:24 +0000

    added hue, saturation, contrast and scale controls

--- alRemap/alRemapColor.cpp
+++ alRemap/alRemapColor.cpp
@@ -7,14 +7,28 @@ enum alRemapParams
 {
 	p_input,
 	p_gamma,
+	p_saturation,
+	p_hueOffset,
+	p_contrast,
+	p_contrastPivot,
+	p_contrastSoftClip,
+	p_scale,
 	p_exposure,
+	p_mask,
 };
 
 node_parameters
 {
 	AiParameterRGB("input", 0.18f, 0.18f, 0.18f);
 	AiParameterFLT("gamma", 1.0f);
+	AiParameterFLT("saturation", 1.0f);
+	AiParameterFLT("hueOffset", 0.0f);
+	AiParameterFLT("contrast", 1.0f);
+	AiParameterFLT("contrastPivot", 0.18f);
+	AiParameterFLT("contrastSoftClip", 0.0f);
+	AiParameterFLT("scale", 1.0f);
 	AiParameterFLT("exposure", 0.f);
+	AiParameterFLT("mask", 1.0f);
 }
 
 node_loader
@@ -47,10 +61,52 @@ shader_evaluate
 {
 	AtRGB input = AiShaderEvalParamRGB(p_input);
 	AtFloat gamma = AiShaderEvalParamFlt(p_gamma);
+	AtFloat saturation = AiShaderEvalParamFlt(p_saturation);
+	AtFloat hueOffset = AiShaderEvalParamFlt(p_hueOffset);
+	AtFloat contrastVal = AiShaderEvalParamFlt(p_contrast);
+	AtFloat contrastPivot = AiShaderEvalParamFlt(p_contrastPivot);
+	AtFloat contrastSoftClip = AiShaderEvalParamFlt(p_contrastSoftClip);
+	AtFloat scale = AiShaderEvalParamFlt(p_scale);
 	AtFloat exposure = AiShaderEvalParamFlt(p_exposure);
+	AtFloat mask = AiShaderEvalParamFlt(p_mask);
 
-	AtRGB result = pow(input, 1.0f/gamma);
-	result = result * powf(2.0f, exposure);
+	AtRGB result = input;
+	if (mask > 0.0f)
+	{
+		// gamma
+		result = pow(input, 1.0f/gamma);
+
+		// saturation
+		if (saturation != 1.0f)
+		{
+			float l = luminance(result);
+			result = lerp(rgb(l), result, saturation);
+		}
+
+		// hue
+		if (hueOffset != 0.0f)
+		{
+			AtRGB hsv = rgb2hsv(result);
+			hsv.r += hueOffset;
+			hsv.r = fmod(hueOffset, 1.0f);
+			result = hsv2rgb(hsv);
+		}
+
+		// contrast
+		if (contrastVal != 1.0f)
+		{
+			result = contrast(result, contrastVal, contrastPivot, contrastSoftClip);
+		}
+
+		// gain and exposure
+		result = result * powf(2.0f, exposure) * scale;
+
+		// mask
+		if (mask < 1.0f)
+		{
+			result = lerp(input, result, mask);
+		}
+	}
 	sg->out.RGB = result;
 }
 
