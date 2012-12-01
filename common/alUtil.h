e7f20a3c9f5308c7a32d4cc493de45255061d6a7
Anders Langlands <anderslanglands@gmail.com>
2012-12-01 12:51:28 +0000

    added missing functions for cloth

--- common/alUtil.h
+++ common/alUtil.h
@@ -404,6 +404,62 @@ inline std::ostream& operator<<( std::ostream& os, AtVector v )
    return os;
 }
 
+
+/// Always-positive modulo function (assumes b > 0)
+inline int modulo(int a, int b)
+{
+	int r = a % b;
+	return (r < 0) ? r+b : r;
+}
+
+/// Always-positive modulo function, float version (assumes b > 0)
+inline AtFloat modulo(AtFloat a, AtFloat b) {
+	AtFloat r = fmodf(a, b);
+	return (r < 0) ? r+b : r;
+}
+
+/**
+ * \ref Generate fast and reasonably good pseudorandom numbers using the
+ * Tiny Encryption Algorithm (TEA) by David Wheeler and Roger Needham.
+ *
+ * For details, refer to "GPU Random Numbers via the Tiny Encryption Algorithm"
+ * by Fahad Zafar, Marc Olano, and Aaron Curtis.
+ *
+ * \param v0
+ *     First input value to be encrypted (could be the sample index)
+ * \param v1
+ *     Second input value to be encrypted (e.g. the requested random number dimension)
+ * \param rounds
+ *     How many rounds should be executed? The default for random number
+ *     generation is 4.
+ * \return
+ *     A uniformly distributed 64-bit integer
+ */
+inline AtUInt64 sampleTEA(AtUInt32 v0, AtUInt32 v1, int rounds = 4) {
+	AtUInt32 sum = 0;
+
+	for (int i=0; i<rounds; ++i)
+	{
+		sum += 0x9e3779b9;
+		v0 += ((v1 << 4) + 0xA341316C) ^ (v1 + sum) ^ ((v1 >> 5) + 0xC8013EA4);
+		v1 += ((v0 << 4) + 0xAD90777D) ^ (v0 + sum) ^ ((v0 >> 5) + 0x7E95761E);
+	}
+
+	return ((AtUInt64) v1 << 32) + v0;
+}
+
+inline AtFloat sampleTEAFloat(AtUInt32 v0, AtUInt32 v1, int rounds = 4) {
+	/* Trick from MTGP: generate an uniformly distributed
+       single precision number in [1,2) and subtract 1. */
+    union
+    {
+    	AtUInt32 u;
+		float f;
+    } x;
+    x.u = ((sampleTEA(v0, v1, rounds) & 0xFFFFFFFF) >> 9) | 0x3f800000UL;
+    return x.f - 1.0f;
+}
+
 // TODO: make this better
 #define M_RAN_INVM32 2.32830643653869628906e-010
 inline double random(AtUInt32 ui) { return ui * M_RAN_INVM32; }
