cddb3a706e4bd294c8ff3e32e7ce480ecddf7122
Anders Langlands <anderslanglands@gmail.com>
2013-03-04 19:10:39 -0800

    swicthed diffuse parameters to hair cllour and density

--- alHair/alHairTemplate.py
+++ alHair/alHairTemplate.py
@@ -12,25 +12,27 @@ class AEalHairTemplate(alShadersTemplate):
         #self.endLayout() # end Diffuse
 
         self.beginLayout('Fibre properties', collapse=False)
+        self.addControl('hairColor', label='Color')
+        self.addControl('hairColorDensity', label='Color density')
         self.addControl('specularWidth', label='Highlight width')
         self.addControl('specularShift', label='Highlight shift')
         self.endLayout() # end Fibre properties
 
         self.beginLayout('Specular 1', collapse=False)
         self.addControl('specular1Strength', label='Strength')
-        self.addControl('specular1Color', label='Color')
+        self.addControl('specular1Color', label='Tint')
         self.endLayout() # end Specular 1
 
         self.beginLayout('Specular 2', collapse=False)
         self.addControl('specular2Strength', label='Strength')
-        self.addControl('specular2Color', label='Color')
+        self.addControl('specular2Color', label='Tint')
         self.addControl('glintStrength', label='Glint strength')
         self.addControl('glintRolloff', label='Glint rolloff')
         self.endLayout() # end Specular 2
 
         self.beginLayout('Transmission', collapse=False)
         self.addControl('transmissionStrength', label='Strength')
-        self.addControl('transmissionColor', label='Color')
+        self.addControl('transmissionColor', label='Tint')
         self.addControl('transmissionRolloff', label='Rolloff')
         self.endLayout() # end Transmission
 
