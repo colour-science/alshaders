05783865abb25d54e1b9e5872d064243797a5c08
Anders Langlands <anderslanglands@gmail.com>
2013-03-09 23:16:15 -0800

    got light groups working. now for multiple samples...

--- alLightGroups/alLightGroups.cpp
+++ alLightGroups/alLightGroups.cpp
@@ -1,6 +1,20 @@
 #include <ai.h>
+#include <map>
 
 AI_SHADER_NODE_EXPORT_METHODS(alLightGroups)
+
+#define NUM_LIGHT_GROUPS 8
+static const char* lightGroupNames[NUM_LIGHT_GROUPS] =
+{
+    "lightGroup1",
+    "lightGroup2",
+    "lightGroup3",
+    "lightGroup4",
+    "lightGroup5",
+    "lightGroup6",
+    "lightGroup7",
+    "lightGroup8"
+};
 
 enum alLightGroupParams
 {
@@ -10,6 +24,7 @@ enum alLightGroupParams
 struct ShaderData
 {
     AtSampler* sampler;
+    std::map<AtNode*, int> lightGroups;
 };
 
 node_parameters
@@ -53,6 +68,15 @@ node_update
     AtNode *options   = AiUniverseGetOptions();
     AiSamplerDestroy(data->sampler);
     data->sampler = AiSampler(AiNodeGetInt(options, "GI_diffuse_samples"), 2);
+    // Get all the light nodes in the scene and try and find their light group parameter
+    // we'll store this based on the light pointer for fast access during rendering
+    AtNodeIterator* it = AiUniverseGetNodeIterator(AI_NODE_LIGHT);
+    while (!AiNodeIteratorFinished(it))
+    {
+        AtNode* light = AiNodeIteratorGetNext(it);
+        data->lightGroups[light] = AiNodeGetInt(light, "lightGroup") - 1;
+    }
+    AiNodeIteratorDestroy(it);
 }
 
 shader_evaluate
@@ -66,11 +90,13 @@ shader_evaluate
     ShaderData *data = (ShaderData*)AiNodeGetLocalData(node);
 
     AtRGB* deepGroupPtr = NULL;
+    AtRGB result_directGroup[NUM_LIGHT_GROUPS];
+    for (int i=0; i < NUM_LIGHT_GROUPS; ++i) result_directGroup[i] = AI_RGB_BLACK;
     if (sg->Rt & AI_RAY_CAMERA)
     {
         // if this is a camera ray allocate the group storage
-        deepGroupPtr = (AtRGB*)AiShaderGlobalsQuickAlloc(sg, sizeof(AtRGB));
-        memset(deepGroupPtr, 0, sizeof(AtRGB));
+        deepGroupPtr = (AtRGB*)AiShaderGlobalsQuickAlloc(sg, sizeof(AtRGB)*NUM_LIGHT_GROUPS);
+        memset(deepGroupPtr, 0, sizeof(AtRGB)*NUM_LIGHT_GROUPS);
         AiStateSetMsgPtr("als_deepGroupPtr", deepGroupPtr);
     }
     else
@@ -87,6 +113,9 @@ shader_evaluate
         L = AiEvaluateLightSample(sg, mis,AiOrenNayarMISSample,AiOrenNayarMISBRDF, AiOrenNayarMISPDF);
 
         result_direct += L * color;
+        int lightGroup = data->lightGroups[sg->Lp];
+        if (lightGroup >= 0 && lightGroup < NUM_LIGHT_GROUPS)
+            result_directGroup[lightGroup] += L * color;
     }
     
     double u[2];
@@ -105,12 +134,20 @@ shader_evaluate
             AtRGB f = AiOrenNayarMISBRDF(mis, &wir.dir) / p;
             L = scrs.color * f * color;
             result_indirect += L;
-            *deepGroupPtr = *deepGroupPtr * f * color + result_direct;
+            for (int i=0; i < NUM_LIGHT_GROUPS; ++i)
+            {
+                deepGroupPtr[i] = deepGroupPtr[i] * f * color + result_directGroup[i];    
+            }
+            
         }
     }
     result_indirect *= AiSamplerGetSampleInvCount(sit);
     
+    for (int i = 0; i < NUM_LIGHT_GROUPS; ++i)
+    {
+        AiAOVSetRGB(sg, lightGroupNames[i], deepGroupPtr[i]);
+    }
 
-    //sg->out.RGB = result_direct + result_indirect;
-    sg->out.RGB = *deepGroupPtr;
+    sg->out.RGB = result_direct + result_indirect;
+    //sg->out.RGB = *deepGroupPtr;
 }
