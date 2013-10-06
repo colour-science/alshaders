7439741295cb444d3577ddcebc05c8864e2ee062
Anders Langlands <anderslanglands@gmail.com>
2013-10-06 17:57:15 -0400

    added automatic ui generation for alSurface

---
+++ alSurface/alSurface.ui.py
@@ -0,0 +1,136 @@
+import uigen
+print "running uigen_test.py"
+
+ui.name = 'alSurface'
+ui.setDescription('General-purpose, physically plausible surface shader')
+ui.setMayaClassification('shader/surface')
+ui.setOutput('rgb')
+ui.maya_id = '0x00116402'
+ui.maya_swatch = True
+ui.maya_matte = True
+ui.maya_bump = True
+ui.soft_name = 'ALS_Surface'
+ui.soft_classification = 'material'
+
+with uigen.group(ui, 'Diffuse', False):
+	ui.parameter(name='diffuseStrength', ptype='float', label='Strength', mn=0, mx=1, description='Multiplier on the diffuse result')
+	ui.parameter('diffuseColor', 'rgb', 'Color')
+	ui.parameter('diffuseRoughness', 'float', 'Roughness', mn=0, mx=1)
+
+	with uigen.group(ui, 'Backlight'):
+		ui.parameter('backlightStrength', 'float', 'Strength', mn=0, mx=1)
+		ui.parameter('backlightColor', 'rgb', 'Color')
+
+	with uigen.group(ui, 'SSS'):
+		ui.parameter('sssMix', 'float', 'Mix', mn=0, mx=1)
+		ui.parameter('sssRadius', 'float', 'Distance', mn=0)
+		ui.parameter('sssRadiusColor', 'rgb', 'Color')
+		ui.parameter('sssDensityScale', 'float', 'Density scale', mn=0)
+
+	with uigen.group(ui, 'Advanced'):
+		ui.parameter('diffuseExtraSamples', 'int', 'Extra samples', smn=-5, smx=5)
+		ui.parameter('diffuseEnableCaustics', 'bool', 'Enable caustics')
+# end Diffuse
+
+with uigen.group(ui, 'Specular 1', False):
+	ui.parameter('specular1Strength', 'float', 'Strength', mn=0, mx=1)
+	ui.parameter('specular1Color', 'rgb', 'Color')
+	ui.parameter('specular1Roughness', 'float', 'Roughness', mn=0, mx=1)
+	ui.parameter('specular1Ior', 'float', 'IOR', mn=1)
+
+	with uigen.group(ui, 'Advanced'):
+		ui.parameter('specular1RoughnessDepthScale', 'float', 'Roughness depth scale', connectible=False)
+		ui.parameter('specular1ExtraSamples', 'int', 'Extra samples', smn=-5, smx=5)
+		ui.parameter('specular1Normal', 'vector', 'Normal')
+# end Specular 1
+
+with uigen.group(ui, 'Specular 2'):
+	ui.parameter('specular2Strength', 'float', 'Strength', mn=0, mx=1)
+	ui.parameter('specular2Color', 'rgb', 'Color')
+	ui.parameter('specular2Roughness', 'float', 'Roughness', mn=0, mx=1)
+	ui.parameter('specular2Ior', 'float', 'IOR', mn=1)
+
+	with uigen.group(ui, 'Advanced'):
+		ui.parameter('specular2RoughnessDepthScale', 'float', 'Roughness depth scale', connectible=False)
+		ui.parameter('specular2ExtraSamples', 'int', 'Extra samples', smn=-5, smx=5)
+		ui.parameter('specular2Normal', 'vector', 'Normal')
+# end Specular 2
+
+with uigen.group(ui, 'Transmission'):
+	ui.parameter('transmissionStrength', 'float', 'Strength', mn=0, mx=1)
+	ui.parameter('transmissionColor', 'rgb', 'Color')
+	ui.parameter('transmissionLinkToSpecular1', 'bool', 'Link to specular 1')
+	ui.parameter('transmissionRoughness', 'float', 'Roughness', mn=0, mx=1)
+	ui.parameter('transmissionIor', 'float', 'IOR', mn=1)
+
+	with uigen.group(ui, 'Scattering'):
+		ui.parameter('ssStrength', 'float', 'Strength', mn=0, mx=1)
+		ui.parameter('ssTargetColor', 'rgb', 'Color')
+		ui.parameter('ssDirection', 'float', 'Direction', mn=-1, mx=1)
+		ui.parameter('ssBalance', 'float', 'Balance', mn=0, mx=1)
+		ui.parameter('ssInScattering', 'bool', 'In-scattering')
+		ui.parameter('ssDensityScale', 'float', 'Density scale')
+		ui.parameter('ssSpecifyCoefficients', 'bool', 'Specify coefficients')
+		ui.parameter('ssScattering', 'float', 'Scattering')
+		ui.parameter('ssAbsorption', 'float', 'Absorption')
+
+	with uigen.group(ui, 'Advanced'):
+		ui.parameter('transmissionRoughnessDepthScale', 'float', 'Roughness depth scale')
+		ui.parameter('transmissionExtraSamples', 'int', 'Extra samples', smn=-5, smx=5)
+		ui.parameter('transmissionEnableCaustics', 'bool', 'Enable internal reflections')
+		ui.parameter('rrTransmission', 'bool', 'RR')
+		ui.parameter('rrTransmissionDepth', 'int', 'RR depth')
+# end Transmission
+
+with uigen.group(ui, 'Emission'):
+	ui.parameter('emissionStrength', 'float', 'Strength', mn=0)
+	ui.parameter('emissionTargetColor', 'rgb', 'Color')
+# end Emission
+
+with uigen.group(ui, 'IDs'):
+	for i in range(1,9):
+		ui.parameter('id%d'%i, 'rgb')
+# end IDs
+
+with uigen.group(ui, 'AOVs'):
+	ui.parameter('lightGroupsIndirect', 'bool', 'Indirect light groups')
+	ui.parameter('standardCompatibleAOVs', 'bool', 'Write standard AOVs only')
+	ui.parameter('transmitAovs', 'bool', 'Transmit AOVs')
+	ui.aov('aov_diffuse_color', 'rgb', 'Diffuse color')
+	ui.aov('aov_direct_diffuse', 'rgb', 'Direct diffuse')
+	ui.aov('aov_direct_diffuse_raw', 'rgb', 'Direct diffuse (raw)')
+	ui.aov('aov_indirect_diffuse', 'rgb', 'Indirect diffuse')
+	ui.aov('aov_indirect_diffuse_raw', 'rgb', 'Indirect diffuse (raw)')
+	ui.aov('aov_direct_backlight', 'rgb', 'Direct backlight')
+	ui.aov('aov_indirect_backlight', 'rgb', 'Indirect backlight')
+	ui.aov('aov_direct_specular', 'rgb', 'Direct specular')
+	ui.aov('aov_indirect_specular', 'rgb', 'Indirect specular')
+	ui.aov('aov_direct_specular_2', 'rgb', 'Direct specular 2')
+	ui.aov('aov_indirect_specular_2', 'rgb', 'Indirect specular 2')
+	ui.aov('aov_single_scatter', 'rgb', 'Single scatter')
+	ui.aov('aov_sss', 'rgb', 'SSS')
+	ui.aov('aov_refraction', 'rgb', 'Refraction')
+	ui.aov('aov_emission', 'rgb', 'Emission')
+	ui.aov('aov_uv', 'rgb', 'UV')
+	ui.aov('aov_depth', 'rgb', 'Depth')
+	ui.aov('aov_light_group_1', 'rgb', 'Light group [1]')
+	ui.aov('aov_light_group_2', 'rgb', 'Light group [2]')
+	ui.aov('aov_light_group_3', 'rgb', 'Light group [3]')
+	ui.aov('aov_light_group_4', 'rgb', 'Light group [4]')
+	ui.aov('aov_light_group_5', 'rgb', 'Light group [5]')
+	ui.aov('aov_light_group_6', 'rgb', 'Light group [6]')
+	ui.aov('aov_light_group_7', 'rgb', 'Light group [7]')
+	ui.aov('aov_light_group_8', 'rgb', 'Light group [8]')
+	ui.aov('aov_id_1', 'rgb', 'ID [1]')
+	ui.aov('aov_id_2', 'rgb', 'ID [2]')
+	ui.aov('aov_id_3', 'rgb', 'ID [3]')
+	ui.aov('aov_id_4', 'rgb', 'ID [4]')
+	ui.aov('aov_id_5', 'rgb', 'ID [5]')
+	ui.aov('aov_id_6', 'rgb', 'ID [6]')
+	ui.aov('aov_id_7', 'rgb', 'ID [7]')
+	ui.aov('aov_id_8', 'rgb', 'ID [8]')
+
+
+
+
+
---
+++ uigen.py
@@ -0,0 +1,497 @@
+#! /usr/bin/env python
+
+import sys
+import uuid
+
+def enum(*sequential, **named):
+	enums = dict(zip(sequential, range(len(sequential))), **named)
+	reverse = dict((value, key) for key, value in enums.iteritems())
+	enums['reverse_mapping'] = reverse
+	return type('Enum', (), enums)
+
+class UiElement:
+	name = ''
+	description = None
+	parent = None
+
+class Group(UiElement):
+	collapse = True
+	children = None
+
+	def __init__(self, name, collapse=True, description=None):
+		self.name = name
+		self.collapse = collapse
+		self.description = description
+
+	def __str__(self):
+		return 'Group %s' % self.name
+
+
+class Tab(Group):
+	def __init__(self, name, collapse=True, description=None):
+		self.name = name
+		self.collapse = collapse
+		self.description = description
+
+	def __str__(self):
+		return 'Tab %s' % self.name
+
+class Parameter(UiElement):
+	ptype = None
+	label = None
+	mn = None
+	mx = None
+	smn = None
+	smx = None
+	connectible = True
+
+
+	def __init__(self, name, ptype, label=None, description=None, mn=None, mx=None, smn=None, smx=None, connectible=True):
+		self.name = name
+		self.ptype = ptype
+		self.description = description
+		if not label:
+			label = name
+		self.label = label
+		self.mn = mn
+		self.mx = mx
+		self.smn = smn
+		self.smx = smx
+
+		if ptype == 'bool' or ptype == 'string' or ptype == 'int':
+			self.connectible = False
+
+	def __str__(self):
+		return 'Parameter %s %s' % (self.name, self.ptype)
+
+class AOV(Parameter):
+	def __init__(self, name, ptype, label=None):
+		self.name = name
+		self.ptype = ptype
+		self.label = label
+		self.connectible = False
+
+	def __str__(self):
+		return 'AOV %s %s' % (self.name, self.ptype)
+
+class ShaderDef:
+	name = None
+	description = None
+	output = None
+	maya_name = None
+	maya_classification = None
+	maya_id = None
+	maya_swatch = False
+	maya_bump = False
+	maya_matte = False
+	soft_name = None
+	soft_classification = None
+	soft_version = 1
+	hierarchy_depth = 0
+	root = None
+	current_parent = None
+	parameters = []
+	aovs = []
+
+	def __init__(self):
+		self.root = Group('__ROOT__', False)
+		self.current_parent = self.root
+
+	def setMayaClassification(self, mc):
+		self.maya_classification = mc
+
+	def setDescription(self, d):
+		self.description = d
+
+	def setOutput(self, o):
+		self.output = o
+
+	def beginGroup(self, name, collapse=True, description=''):
+		g = None
+		if self.current_parent is self.root:
+			g = Tab(name, collapse, description)
+		else:
+			g = Group(name, collapse, description)
+
+		if not self.current_parent.children:
+			self.current_parent.children = [g]
+		else:
+			self.current_parent.children.append(g)
+
+		g.parent = self.current_parent
+		self.current_parent = g
+
+	def endGroup(self):
+		self.current_parent = self.current_parent.parent
+
+	def parameter(self, name, ptype, label=None, description=None, mn=None, mx=None, smn=None, smx=None, connectible=None):
+		p = Parameter(name, ptype, label, description, mn, mx, smn, smx, connectible)
+		if not self.current_parent.children:
+			self.current_parent.children = [p]
+		else:
+			self.current_parent.children.append(p)
+
+		p.parent = self.current_parent
+		self.parameters.append(p)
+
+	def aov(self, name, ptype, label=None, description=None):
+		p = AOV(name, ptype, label)
+		if not self.current_parent.children:
+			self.current_parent.children = [p]
+		else:
+			self.current_parent.children.append(p)
+
+		p.parent = self.current_parent
+		self.aovs.append(p)
+
+	def __str__(self):
+		return 'ShaderDef %s' % self.name
+
+class group:
+	name = ''
+	collapse = True
+	description = ''
+	ui = None
+
+	def __init__(self, ui, name, collapse=True, description=None):
+		self.ui = ui
+		self.name = name
+		self.collapse = collapse
+		self.description = description
+
+	def __enter__(self):
+		self.ui.beginGroup(self.name, self.collapse, self.description)
+
+	def __exit__(self, type, value, traceback):
+		self.ui.endGroup()
+
+
+def DebugPrintElement(el, d):
+	indent = ''
+	for i in range(d):
+		indent += '\t'
+	print '%s %s' % (indent, el)
+	if isinstance(el, Group) and el.children:
+		print '%s has %d children' % (el, len(el.children))
+		for e in el.children:
+			DebugPrintElement(e, d+1)
+		d -= 1
+
+def DebugPrintShaderDef(sd):
+	print '%s' % sd
+	DebugPrintElement(sd.root, 0)
+
+def writei(f, s, d=0):
+	indent = ''
+	for i in range(d):
+		indent += '\t'
+	f.write('%s%s\n' %(indent, s))
+
+def WalkAETemplate(el, f, d):
+	if isinstance(el, Group):
+		writei(f, 'self.beginLayout("%s", collapse=%r)' % (el.name, el.collapse), d)
+
+		if el.children:
+			for e in el.children:
+				WalkAETemplate(e, f, d)
+
+		writei(f, 'self.endLayout() # END %s' % el.name, d)
+
+	elif isinstance(el, Parameter):
+		writei(f, 'self.addControl("%s", label="%s")' % (el.name, el.label), d)
+
+def WriteAETemplate(sd, fn):
+	f = open(fn, 'w')
+	writei(f, 'import pymel.core as pm', 0)
+	writei(f, 'from alShaders import alShadersTemplate\n', 0)
+
+	writei(f, 'class AE%sTemplate(alShadersTemplate):' % sd.name, 0)
+	writei(f, 'def setup(self):', 1)
+
+	if sd.maya_swatch:
+		writei(f, 'self.addSwatch()', 2)
+
+	writei(f, 'self.beginScrollLayout()', 2) # begin main scrollLayout
+	writei(f, '')
+
+	if sd.maya_matte:
+		writei(f, 'self.beginLayout("Matte")', 2)
+		writei(f, 'self.addControl("aiEnableMatte", label="Enable Matte")', 2)
+		writei(f, 'self.addControl("aiMatteColor", label="Matte Color")', 2)
+		writei(f, 'self.addControl("aiMatteColorA", label="Matte Opacity")', 2)
+		writei(f, 'self.endLayout()', 2)
+
+
+	for e in sd.root.children:
+		WalkAETemplate(e, f, 2)
+
+	if sd.maya_bump:
+		writei(f, 'self.addBumpLayout()', 2)		
+
+	writei(f, '')
+	writei(f, 'pm.mel.AEdependNodeTemplate(self.nodeName)', 2)
+
+	writei(f, 'self.addExtraControls()', 2)
+
+	writei(f, '')
+	writei(f, 'self.endScrollLayout()', 2)	#end main scrollLayout
+
+	f.close()
+
+def WriteMTDParam(f, name, ptype, value, d):
+	if value == None:
+		return
+
+	if ptype == 'bool':
+		if value:
+			bval = "TRUE"
+		else:
+			bval = "FALSE"
+		writei(f, '%s BOOL %s' % (name, bval), d)
+	elif ptype == 'int':
+		writei(f, '%s INT %d' % (name, value), d)
+	elif ptype == 'float':
+		writei(f, '%s FLOAT %r' % (name, value), d)
+	elif ptype == 'string':
+		writei(f, '%s STRING "%r"' % (name, value), d)
+
+def WriteMTD(sd, fn):
+	f = open(fn, 'w')
+	writei(f, '[node %s]' % sd.name, 0)
+	writei(f, 'desc STRING "%s"' % sd.description, 1)
+	writei(f, 'maya.name STRING "%s"' % sd.name, 1)
+	writei(f, 'maya.classification STRING "%s"' % sd.maya_classification, 1)
+	writei(f, 'maya.id INT %s' % sd.maya_id, 1)
+	writei(f, '')
+
+	for p in sd.parameters:
+		writei(f, '[attr %s]' % p.name, 1)
+		WriteMTDParam(f, "min", "float", p.mn, 2)
+		WriteMTDParam(f, "max", "float", p.mx, 2)
+		WriteMTDParam(f, "softmin", "float", p.smn, 2)
+		WriteMTDParam(f, "softmax", "float", p.smx, 2)
+		WriteMTDParam(f, "desc", "string", p.description, 2)
+		WriteMTDParam(f, "connectible", "bool", True, 2)
+
+	for a in sd.aovs:
+		writei(f, '[attr %s]' % a.name, 1)
+		writei(f, 'aov.type INT 0x05', 2)
+		writei(f, 'aov.enable_composition BOOL TRUE', 2)
+		writei(f, 'default STRING "%s"' % a.name[4:], 2)
+
+	f.close()
+
+def getSPDLTypeName(t):
+	if t == 'bool':
+		return 'boolean'
+	elif t == 'int':
+		return 'integer'
+	elif t == 'float':
+		return 'scalar'
+	elif t == 'rgb':
+		return 'color'
+	else:
+		return t
+
+def WriteSPDLParameter(f, p):
+	writei(f, 'Parameter "%s" input' % p.name, 1)
+	writei(f, '{', 1)
+	writei(f, 'GUID = "{%s}";' % uuid.uuid4(), 2)
+	if isinstance(p, AOV):
+		writei(f, 'Type = string;', 2)
+		si_aov_name = p.name.replace('aov_', 'arnold_').title()
+		writei(f, 'Value = "%s";' % si_aov_name, 2)
+	else:
+		writei(f, 'Type = %s;' % getSPDLTypeName(p.ptype), 2)
+		if p.connectible:
+			writei(f, 'Texturable = on;', 2)
+		else:
+			writei(f, 'Texturable = off;', 2)
+	writei(f, '}', 1)
+
+def WriteSPDLPropertySet(f, sd):
+	writei(f, 'PropertySet "%s_pset"' % sd.name)
+	writei(f, '{')
+
+
+	writei(f, 'Parameter "out" output', 1)
+	writei(f, '{', 1)
+	writei(f, 'GUID = "{%s}";' % uuid.uuid4(), 2)
+	writei(f, 'Type = %s;' % getSPDLTypeName(sd.output), 2)
+	writei(f, '}', 1)
+
+	for p in sd.parameters:
+		WriteSPDLParameter(f, p)
+
+	for a in sd.aovs:
+		WriteSPDLParameter(f, a)
+
+	writei(f, '}')
+
+def writeSPDLDefault(f, p, i):
+	writei(f, '%s' % p.name, 1)
+	writei(f, '{', 1)
+
+	writei(f, 'Name = "%s";' % p.label, 2)
+	if p.ptype == 'rgb':
+		writei(f, 'UIType = "rgb";', 2)
+	elif p.smn and p.smx:
+		writei(f, 'UIRange = %f To %f;' % (p.smn, p.smx), 2)
+	if p.connectible:
+		writei(f, 'Commands = "{F5C75F11-2F05-11D3-AA95-00AA0068D2C0}";' % i, 2)
+
+	writei(f, '}', 1)
+
+def WalkSPDL(f, el, d, wc):
+
+	if isinstance(el, Group):
+		if isinstance(el, Tab) and wc:
+			writei(f, 'Tab "%s"' % el.name, d)
+		else:
+			writei(f, 'Group "%s"' % el.name, d)
+		writei(f, '{', d)
+		if el.children:
+			for e in el.children:
+				WalkSPDL(f, e, d+1, wc)
+		writei(f, '}', d)
+
+	elif isinstance(el, Parameter):
+		if wc or el.connectible:
+			writei(f, '%s;' % el.name, d)
+
+		
+
+def WriteSPDL(sd, fn):
+	f = open(fn, 'w')
+	writei(f, 'SPDL')
+	writei(f, 'Version = "2.0.0.0";')
+	writei(f, 'Reference = "{%s}";' % uuid.uuid4())
+
+	WriteSPDLPropertySet(f, sd)
+
+	writei(f, 'MetaShader "%s_meta"' % sd.name)
+	writei(f, '{')
+	writei(f, 'Name = "%s";' % sd.soft_name, 1)
+	writei(f, 'Type = %s;' % sd.soft_classification, 1)
+	writei(f, 'Renderer "mental ray"', 1)
+	writei(f, '{', 1)
+	writei(f, 'Name = "%s";' % sd.soft_name, 2)
+	writei(f, 'Filename = "%s";' % sd.name, 2)
+	writei(f, 'Options', 2)
+	writei(f, '{', 2)
+	writei(f, '"version" = 1;', 3)
+	writei(f, '}', 2)
+	writei(f, '}', 1)
+	writei(f, '}')
+
+	# Begin Defaults
+	writei(f, 'Defaults')
+	writei(f, '{')
+
+	cmd = uuid.uuid4()
+
+	for p in sd.parameters:
+		writeSPDLDefault(f, p, cmd)
+
+	for a in sd.aovs:
+		writei(f, '%s' % a.name, 1)
+		writei(f, '{', 1)
+		writei(f, 'Name = "%s";' % a.name, 2)
+		writei(f, '}', 1)
+
+	writei(f, '}')
+	# End Defaults
+
+	#Begin Layout
+	writei(f, 'Layout "Default"')
+	writei(f, '{')
+	
+	for el in sd.root.children:
+		WalkSPDL(f, el, 1, True)
+
+	writei(f, '}')
+	writei(f, 'Layout "RenderTree"')
+	writei(f, '{')
+	
+	for el in sd.root.children:
+		WalkSPDL(f, el, 1, False)
+
+	writei(f, '}')
+	# End Layout
+
+# 	#Begin Logic
+# 	writei(f, 'Logic')
+# 	writei(f, '{')
+	
+# 	if sd.aovs:
+# 		f.write("""
+# 	Sub OnInit
+# 		Dim oRenderChannels
+# 		Set oRenderChannels = ActiveProject.ActiveScene.PassContainer.Properties( "Scene Render Options" ).RenderChannels
+
+# 		If TypeName(oRenderChannels) = "Nothing" Then
+# 			LogMessage "Scene Render Options property not found. Can't enumerate render channels.", siError
+# 			Exit Sub
+# 		End If
+# 		If oRenderChannels.Count = 0 Then
+# 			LogMessage "No render channels defined.", siError
+# 			Exit Sub
+# 		End If
+
+# 		Dim		idx
+# 		ReDim oChannelList( oRenderChannels.Count * 2 + 1 )
+
+# 		idx = 0
+# 		for each oChannel in oRenderChannels
+# 			If oChannel.ChannelType = siRenderChannelColorType And oChannel.UserDefined Then
+# 				oChannelList( idx * 2 + 0 ) = oChannel.Name
+# 				oChannelList( idx * 2 + 1 ) = oChannel.Name
+# 				idx = idx + 1
+# 			End If
+# 		next
+
+# 		ReDim Preserve oChannelList( idx * 2 - 1 )
+
+# 		Dim oChannelCombo
+# """)
+# 		for a in sd.aovs:
+# 			writei(f, 'Set oChannelCombo = PPG.PPGLayout.Item("%s")' % a.name, 2)
+# 			writei(f, 'oChannelCombo.UIItems = oChannelList', 2)
+
+# 		f.write("""
+
+# 		PPG.Refresh
+# 	End Sub
+# """)
+# 	writei(f, '}')
+# 	#End Logic
+
+	writei(f, 'Plugin = Shader')
+	writei(f, '{')
+	writei(f, 'Filename = "%s";' % sd.name, 1)
+	writei(f, '}')
+
+	f.close()
+
+# Main. Load the UI file and build UI templates from the returned structure
+if __name__ == '__main__':
+	if len(sys.argv) != 5:
+		print 'ERROR: must supply exactly ui source input and mtd, ae and spdl outputs'
+		sys.exit(1)
+
+	ui = ShaderDef()
+	globals_dict = {'ui':ui}
+	execfile(sys.argv[1], globals_dict)
+
+	if not isinstance(ui, ShaderDef):
+		print 'ERROR: ui object is not a ShaderDef. Did you assign something else to it by mistake?'
+		sys.exit(2)
+
+	print ui.maya_classification
+	WriteMTD(ui, sys.argv[2])
+	WriteAETemplate(ui, sys.argv[3])
+	WriteSPDL(ui, sys.argv[4])
+
+	
+
+
