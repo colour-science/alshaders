5b3965a89bbcb8f49e02f26d372de7bfda90002c
Espen Nordahl <nordahl@stormstudios.no>
2014-01-06 14:25:29 +0100

    Added CellNoise tiling to the triplanar shader. Still need to add cell softness and clean up the code.

--- alTriplanar/alTriplanar.cpp
+++ alTriplanar/alTriplanar.cpp
@@ -18,9 +18,23 @@ static const char* triplanarSpaceNames[] =
 	NULL
 };
 
+enum TriplanarTiling
+{
+    TM_REGULAR = 0,
+    TM_CELLNOISE
+};
+
+static const char* triplanarTilingNames[] =
+{
+    "regular",
+    "cellnoise",
+    NULL
+};
+
 enum alTriplanarParams
 {
-	p_space,
+    p_space,
+    p_tiling,
     p_texture,
     p_blendSoftness,
     p_scale,
@@ -31,10 +45,11 @@ enum alTriplanarParams
 node_parameters
 {
 	AiParameterENUM("space", 0, triplanarSpaceNames);
+    AiParameterENUM("tiling", 0, triplanarTilingNames);
     AiParameterSTR("texture", "");
     AiParameterFLT("blendSoftness", 0.1);
     AiParameterFLT("scale", 1.0f);
-	AiParameterFLT("offset", 0.0f);
+    AiParameterFLT("offset", 0.0f);
 	AiParameterPnt("P", 0.0f, 0.0f, 0.0f);
 }
 
@@ -78,9 +93,50 @@ node_update
 
 }
 
+inline bool tileCellNoise(float u, float v,  AtRGBA *textureResult, AtShaderGlobals *sg,
+                          AtTextureHandle *handle, AtTextureParams *params){
+        // lookup X
+    AtPoint PX;
+    PX.x = u;
+    PX.y = v;
+    PX.z = 0.f;
+
+    // run cellnoise
+    float f;
+    AtVector delta;
+    AtUInt32 id;
+    AiCellular(PX, 1, 1, 1.92, 1, &f, &delta, &id);
+
+    // pick random direction for orientation
+    AtVector orientVectorX;
+    double phi = random(id) * M_PI * 2;
+    orientVectorX.x = cosf(phi);
+    orientVectorX.y = sinf(phi);
+    orientVectorX.z = 0.f;
+
+    AtVector orientVectorZ;
+    orientVectorZ.x = 0.f;
+    orientVectorZ.y = 0.f;
+    orientVectorZ.z = 1.f;
+
+    AtVector orientVectorY = AiV3Cross(orientVectorX, orientVectorZ);
+
+    AiV3RotateToFrame(delta, orientVectorX, orientVectorY, orientVectorZ);
+
+    // find new uv coordinates
+    sg->u = delta.x;
+    sg->v = delta.y;
+
+    bool success = false;
+    *textureResult = AiTextureHandleAccess(sg, handle, params, &success);
+
+    return success;
+}
+
 shader_evaluate
 {
 	int space = AiShaderEvalParamInt(p_space);
+    int tiling = AiShaderEvalParamInt(p_tiling);
     float blendSoftness = AiShaderEvalParamFlt(p_blendSoftness);
     blendSoftness = CLAMP(blendSoftness, 0.f, 1.f);
     float scale = AiShaderEvalParamFlt(p_scale);
@@ -112,51 +168,89 @@ shader_evaluate
         }
     }
 
-    AtRGBA textureResultX;
-    AtRGBA textureResultY;
-    AtRGBA textureResultZ;
-    bool textureAccessX = false;
-    bool textureAccessY = false;
-    bool textureAccessZ = false;
-
-    // lookup X
-    sg->u = (P.z + 123.94) / scale + offset;
-    sg->v = (P.y + 87.22) / scale + offset;
-    textureResultX = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessX);
-
-    // lookup Y
-    sg->u = (P.z + 9.2) / scale + offset;
-    sg->v = (P.x + 74.1) / scale + offset;
-    textureResultY = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessY);
-
-    // lookup Z
-    sg->u = (P.x - 12.55) / scale + offset;
-    sg->v = (P.y + 6) / scale + offset;
-    textureResultZ = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessZ);
-
-    if(textureAccessX && textureAccessY && textureAccessZ){
-        // blend result
-        AtRGB textureResult = AI_RGB_BLACK;
-        float weights[3] = {fabsf(sg->N.x), fabsf(sg->N.y), fabsf(sg->N.z)};
-        float sum = 0.f;
-        int highest = 0;
-        for(int i=0; i<3; ++i){
-            weights[i] = weights[i] - (1.f-blendSoftness)/2.f;
-            weights[i] = MAX(weights[i], 0.00f);
-            sum += weights[i];
-        }
-        if(sum){
-            for(int i=0; i<3; ++i){
-                weights[i] /= sum;
-            }
-        }
-        textureResult += textureResultX.rgb() * weights[0];
-        textureResult += textureResultY.rgb() * weights[1];
-        textureResult += textureResultZ.rgb() * weights[2];
-        sg->out.RGB = textureResult;
+        // compute blend weights
+    AtRGB textureResult = AI_RGB_BLACK;
+    float weights[3] = {fabsf(sg->N.x), fabsf(sg->N.y), fabsf(sg->N.z)};
+    float weightsum = 0.f;
+    for(int i=0; i<3; ++i){
+        weights[i] = weights[i] - (1.f-blendSoftness)/2.f;
+        weights[i] = MAX(weights[i], 0.00f);
+        weightsum += weights[i];
     }
-    else {
-        sg->out.RGB = AiColorCreate(1.f, 0.0f, 0.f);
+    if(weightsum){
+        for(int i=0; i<3; ++i){
+            weights[i] /= weightsum;
+        }
+    }
+
+
+    if(tiling == TM_CELLNOISE){
+
+        AtRGBA textureResult[3];
+        bool textureAccessX = tileCellNoise((P.y + offset)/scale,
+                                            (P.z + offset)/scale,
+                                            &textureResult[0],
+                                            sg,
+                                            data->texturehandle,
+                                            data->textureparams);
+        bool textureAccessY = tileCellNoise((P.x + offset)/scale,
+                                            (P.z + offset)/scale,
+                                            &textureResult[1],
+                                            sg,
+                                            data->texturehandle,
+                                            data->textureparams);
+        bool textureAccessZ = tileCellNoise((P.y + offset)/scale,
+                                            (P.x + offset)/scale,
+                                            &textureResult[2],
+                                            sg,
+                                            data->texturehandle,
+                                            data->textureparams);
+
+
+        if(textureAccessX && textureAccessY && textureAccessZ){
+            AtRGB result = AI_RGB_BLACK;
+            result += textureResult[0].rgb() * weights[0];
+            result += textureResult[1].rgb() * weights[1];
+            result += textureResult[2].rgb() * weights[2];
+            sg->out.RGB = result;
+        }
+        else {
+            sg->out.RGB = AiColorCreate(1.f, 0.0f, 0.f);
+        }
+
+    } else {
+        // regular tiling
+        AtRGBA textureResultX;
+        AtRGBA textureResultY;
+        AtRGBA textureResultZ;
+        bool textureAccessX = false;
+        bool textureAccessY = false;
+        bool textureAccessZ = false;
+
+        // lookup X
+        sg->u = (P.z + 123.94) / scale + offset;
+        sg->v = (P.y + 87.22) / scale + offset;
+        textureResultX = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessX);
+
+        // lookup Y
+        sg->u = (P.z + 9.2) / scale + offset;
+        sg->v = (P.x + 74.1) / scale + offset;
+        textureResultY = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessY);
+
+        // lookup Z
+        sg->u = (P.x - 12.55) / scale + offset;
+        sg->v = (P.y + 6) / scale + offset;
+        textureResultZ = AiTextureHandleAccess(sg, data->texturehandle, data->textureparams, &textureAccessZ);
+
+        if(textureAccessX && textureAccessY && textureAccessZ){
+            textureResult += textureResultX.rgb() * weights[0];
+            textureResult += textureResultY.rgb() * weights[1];
+            textureResult += textureResultZ.rgb() * weights[2];
+            sg->out.RGB = textureResult;
+        }
+        else {
+            sg->out.RGB = AiColorCreate(1.f, 0.0f, 0.f);
+        }
     }
 
 }
--- alTriplanar/alTriplanar.ui.py
+++ alTriplanar/alTriplanar.ui.py
@@ -1,24 +1,29 @@
 import uigen
 
 ui.shader({
-	'name':'alTriplanar',
+        'name':'alTriplanar',
         'description':'Projects a texture orthographically in three axis.',
-	'output':'rgb',
-	'maya_name':'alTriplanar',
-	'maya_classification':'texture',
-	'maya_id':'0x00116413',
-	'maya_swatch':True,
-	'maya_matte':False,
-	'maya_bump':False,
-	'soft_name':'ALS_Triplanar',
-	'soft_classification':'texture',
-	'soft_version':1
+        'output':'rgb',
+        'maya_name':'alTriplanar',
+        'maya_classification':'texture',
+        'maya_id':'0x00116413',
+        'maya_swatch':True,
+        'maya_matte':False,
+        'maya_bump':False,
+        'soft_name':'ALS_Triplanar',
+        'soft_classification':'texture',
+        'soft_version':1
 })
 
 ui.parameter('space', 'enum', 'world', 'Space', enum_names=[
-	"world",
-	"object",
+        "world",
+        "object",
         "Pref"
+])
+
+ui.parameter('tiling', 'enum', 'regular', 'Tiling', enum_names=[
+        "regular",
+        "cellnoise"
 ])
 
 ui.parameter('texture', 'string', '', 'Texture', connectible=False)
@@ -30,4 +35,3 @@ ui.parameter('scale', 'float', 1.0, 'Scale', connectible=False)
 ui.parameter('offset', 'float', 0.0, 'Offset', connectible=False)
 
 ui.parameter('P', 'vector', (0.0, 0.0, 0.0), 'P')
-
