a477e0aa3d64be152c3ce271bc508618bedaa670
Anders Langlands <anderslanglands@gmail.com>
2014-06-14 18:30:32 +0100

    simplified the packaging scripts

--- package.in.py
+++ package.in.py
@@ -40,57 +40,29 @@ subdirs = [
     'alSurface',
     'alSwitch',
     'alTriplanar',
-    'common'
+    'common',
+    'share'
 ] 
 
 # pattern for files to include in the source distribution
 name_src = 'alShaders-src-%s.%s.%s' % (MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION)
-ptrn_src_src = ['*.cpp', '*.h', '*.txt', '*.py', '*.ui']
-ptrn_src_bin = []
-ptrn_src_ae = None
-ptrn_src_spdl = None
-ptrn_src_args = None
-files_src = ['BUILD_INSTRUCTIONS.txt', 'CMakeLists.txt', 'package.in.py', 'test.in.py', 'README', 'TODO.txt', 'local.cmake.win', 'uigen.py']
+ptrn_src = ['*.cpp', '*.h', '*.txt', '*.py', '*.ui', '*.cmake']
 
-# patterns for files to include in the osx binary distribution
-name_osx = 'alShaders-osx-%s.%s.%s-ai-%s' % (MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION, ARNOLD_VERSION)
-ptrn_osx_src = ['*.py']
-ptrn_osx_bin  = ['*.dylib', '*.mtd']
-ptrn_osx_ae = ['*.py']
-ptrn_osx_spdl = None
-ptrn_osx_args = None
-files_osx = ['BUILD_INSTRUCTIONS.txt', 'README']
+# Binary distribution
+files_src = ['INSTALL', 'CMakeLists.txt', 'package.in.py', 'test.in.py', 'README', 'TODO.txt', 'local.cmake.win', 'uigen.py']
 
-# patterns for files to include in the windows binary distribution
-name_win = 'alShaders-win-%s.%s.%s-ai-%s' % (MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION, ARNOLD_VERSION)
-ptrn_win_src = ['*.py']
-ptrn_win_bin  = ['*.dll', '*.mtd']
-ptrn_win_ae = ['*.py']
-ptrn_win_spdl = ['*.spdl']
-ptrn_win_args = None
-files_win = ['BUILD_INSTRUCTIONS.txt', 'README']
+name_osx = 'alShaders-osx-%s-ai-%s' % (ALS_VERSION, ARNOLD_VERSION)
+name_win = 'alShaders-win-%s-ai-%s' % (ALS_VERSION, ARNOLD_VERSION)
+name_linux = 'alShaders-linux-%s-ai-%s' % (ALS_VERSION, ARNOLD_VERSION)
 
-# patterns for files to include in the linux binary distribution
-name_linux = 'alShaders-linux-%s.%s.%s-ai-%s' % (MAJOR_VERSION,MINOR_VERSION,PATCH_VERSION, ARNOLD_VERSION)
-ptrn_linux_src = ['*.py']
-ptrn_linux_bin  = ['*.so', '*.mtd']
-ptrn_linux_ae = ['*.py']
-ptrn_linux_spdl = ['*.spdl']
-ptrn_linux_args = ['*.args']
-files_linux = ['BUILD_INSTRUCTIONS.txt', 'README']
 
-def copyPatternsToDistDir(subDirs, subDirPrefix, filePatterns, distDir, isSrc=False):
-    for dir in subDirs:
-        if isSrc:
-            destdir = os.path.join(distDir, dir)
-        else:
-            destdir = distDir
+def copyPatternsToDistDir(subDirs, subDirPrefix, filePatterns, distDir):
+    for d in subDirs:
+        destdir = os.path.join(distDir, d)
         mkdir_p(destdir)
-        subdir = os.path.join(subDirPrefix, dir)
+        subdir = os.path.join(subDirPrefix, d)
         for ptrn in filePatterns:
             sdir = subdir
-            if ptrn == '*.dll' and platform.system() == "Windows":
-                sdir = os.path.join(sdir, 'Release')
             for fn in glob.iglob(os.path.join(sdir, ptrn)):
                 shutil.copy(fn, destdir)
                         
@@ -109,28 +81,44 @@ def createArchive(distDir, name, isSrc=False):
         f.add(distDir, arcname = name)
         f.close()
     
-def createDistribution(name, ptrn_src, ptrn_bin, ptrn_ae, ptrn_spdl, ptrn_args, files, isSrc=False):
-    if isSrc:
-        distdir = 'build/%s' % name
-    else:
-        distdir = 'build/dist/%s/%s' % (ALS_VERSION, ARNOLD_VERSION)
+def createSourceDistribution(name, ptrn_src, files):
+    distdir = 'build/%s' % name
     shutil.rmtree(distdir, ignore_errors=True)
     os.mkdir(distdir)
-    if len(ptrn_src):
-        copyPatternsToDistDir(subdirs, '.', ptrn_src, distdir, isSrc)
-    createArchive(distdir, name, isSrc)
+    copyPatternsToDistDir(subdirs, '.', ptrn_src, distdir)
+    copyFilesToDistDir(files_src, distdir)
+    createArchive(distdir, name, True)
+
+def createBinaryDistribution(name, droot):
+    if platform.system() == "Windows":
+        zf = zipfile.ZipFile(os.path.join('..', '%s.zip' % name), 'w')
+        for root, dirs, files in os.walk(droot):
+            rroot = os.path.relpath(root, droot)
+            for f in files:
+                rfile = os.path.join(rroot, f)
+                print rfile
+                zf.write(os.path.join(droot, rfile), arcname=rfile)
+    else:
+        f = tarfile.open(os.path.join('..', '%s.tar.gz' % name), 'w:gz')
+        f.add(droot, arcname = name)
+        f.close()
 
 # source distribution
-createDistribution(name_src, ptrn_src_src, ptrn_src_bin, ptrn_src_ae, ptrn_src_spdl, ptrn_src_args, files_src, True)
+createSourceDistribution(name_src, ptrn_src, files_src)
+
+# Binary distribution
+droot = 'build/dist/%s/ai%s' % (ALS_VERSION, ARNOLD_VERSION)
+createBinaryDistribution(name_osx, droot)
+
 if platform.system() == "Darwin":
     # OS X distribution
-    createDistribution(name_osx, ptrn_osx_src, ptrn_osx_bin, ptrn_osx_ae, ptrn_osx_spdl, ptrn_osx_args, files_osx)
+    createBinaryDistribution(name_osx, droot)
 elif platform.system() == "Windows":
     # Windows
-    createDistribution(name_win, ptrn_win_src, ptrn_win_bin, ptrn_win_ae, ptrn_win_spdl, ptrn_win_args, files_win)
+    createBinaryDistribution(name_win, droot)
 elif platform.system() == "Linux":
     # Linux
-    createDistribution(name_linux, ptrn_linux_src, ptrn_linux_bin, ptrn_linux_ae, ptrn_linux_spdl, ptrn_linux_args, files_linux)
+    createBinaryDistribution(name_linux, droot)
 else:
     print 'Warning: unknown system "%s", not creating binary package' % platform.system()
 
